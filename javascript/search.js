317f0ae1676218e1ece1be9b71157a5171bc86ef
Kelian Halleray <kelianhalleray@gmail.com>
2023-08-17 02:05:55 +0100

    Ajout du filtrage sur les filtres selectionnables

--- javascript/search.js
+++ javascript/search.js
@@ -1,6 +1,8 @@
 import { createRecipeCards } from "./showData.js";
 import { recipes } from '../data/recipes.js';
 import { showDataFilter } from "./showData.js";
+import { itemClicked } from './selectedFilterAnim.js';
+import { crossIconClick } from './selectedFilterAnim.js';
 
 function searchFilter(event) {
   const searchInput = event.target;
@@ -90,8 +92,81 @@ export function filteredRecipes() {
     filteredRecipes.push(recipe);
   });
 
-  showDataFilter(filteredRecipes, activeFilters); 
+  const recipeContainer = document.getElementById('recipeContainer');
+  const recipeCountElement = document.getElementById('totalRecettes');
+  const ingredientsList = document.querySelector('.ingredientsList');
+  const appareilsList = document.querySelector('.appareilsList');
+  const ustensilesList = document.querySelector('.ustensilesList');
+
+  if (filteredRecipes.length === 0) {
+    hideFilterLists([ingredientsList, appareilsList, ustensilesList]);
+    updateNoResultsUI(recipeCountElement, recipeContainer);
+  } else {
+    showFilterLists([ingredientsList, appareilsList, ustensilesList]);
+    updateResultsUI(filteredRecipes, recipeCountElement);
+  }
+}
+
+function hideFilterLists(filterLists) {
+  filterLists.forEach(filterList => {
+    filterList.style.display = 'none';
+  });
+}
+
+function showFilterLists(filterLists) {
+  filterLists.forEach(filterList => {
+    filterList.style.display = 'block';
+  });
+}
+
+function updateNoResultsUI(recipeCountElement, recipeContainer) {
+  recipeCountElement.textContent = '0 recette';
+  recipeContainer.innerHTML = `<p class="no-result-card">Aucun résultat trouvé</p>`;
+}
+
+function updateResultsUI(filteredRecipes, recipeCountElement) {
+  const ingredientsList = document.querySelector('.ingredientsList');
+  const appareilsList = document.querySelector('.appareilsList');
+  const ustensilesList = document.querySelector('.ustensilesList');
+
+  showDataFilter(filteredRecipes);
   createRecipeCards(filteredRecipes);
+  resetFilterItems(filteredRecipes, [ingredientsList, appareilsList, ustensilesList]);
+  recipeCountElement.textContent = `${filteredRecipes.length} recettes`;
+}
+
+function resetFilterItems(filteredRecipes, filterLists) {
+  filterLists.forEach(filterList => {
+    filterList.style.display = 'none';
+
+    const filterItems = filterList.querySelectorAll("li");
+    filterItems.forEach(filter => {
+      const filterText = filter.textContent.toLowerCase();
+      const isSelected = filteredRecipes.some(recipe =>
+        recipe.ingredients.some(ingredient => ingredient.ingredient.toLowerCase() === filterText) ||
+        recipe.appliance.toLowerCase() === filterText ||
+        recipe.ustensils.some(ustensil => ustensil.toLowerCase() === filterText)
+      );
+
+      filter.classList.toggle("selected", isSelected);
+
+      if (isSelected) {
+        filter.removeEventListener("click", itemClicked);
+        const crossIcon = document.createElement("i");
+        crossIcon.style.cursor = "pointer";
+        crossIcon.classList.add("fa-solid", "fa-circle-xmark");
+        crossIcon.addEventListener("click", crossIconClick);
+        filter.appendChild(crossIcon);
+      } else {
+        filter.addEventListener("click", itemClicked);
+        const crossIcon = filter.querySelector("i.fa-circle-xmark");
+        if (crossIcon) {
+          crossIcon.removeEventListener("click", crossIconClick);
+          crossIcon.remove();
+        }
+      }
+    });
+  });
 }
 
 const searchInputMain = document.querySelector('.search');
